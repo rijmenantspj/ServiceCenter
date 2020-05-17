43c9e9947003afb85f2d1d9236b23c2715b68878
Pieter-Jan <rijmenants.pj@gmail.com>
2020-05-17 13:42:45 +0200

    Added Dashboard to Flask App

--- ServiceCenter/__init__.py
+++ ServiceCenter/__init__.py
@@ -28,4 +28,9 @@ def create_app(test_config=None):
     from . import DatabaseManager
     DatabaseManager.init_app(app)
 
+    # Import and register the Dashboard (Blueprint).
+    from . import Dashboard
+    app.register_blueprint(Dashboard.bp)
+    app.add_url_rule('/', endpoint='dashboard')
+
     return app
